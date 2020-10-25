01ab38045ef51f1321c6db9a9c46cf82fb725e74
Ubuntu <ubuntu@ip-172-31-95-228.ec2.internal>
2020-10-25 22:26:55 +0000

    arquivo inicial

---
+++ test1.py
@@ -0,0 +1,16 @@
+import rospy
+from std_msgs.msg import String
+
+rospy.init_node('test1')
+
+def timerCallBack(event):
+    msg = String()
+    msg.data = 'test'
+    pub.publish(msg)
+    
+
+pub = rospy.Publisher('/topic1', String, queue_size=1)
+timer = rospy.Timer(rospy.Duration(0.1), timerCallBack)
+
+rospy.spin()
+
